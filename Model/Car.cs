73711a5014f172dd1cd6380322bc6a41b19a26c4
Farkas <farasz@kkszki.hu>
2024-09-26 10:23:08 +0200

    Car class is ready to use.

---
+++ Model/Car.cs
@@ -0,0 +1,11 @@
+﻿namespace AutoSale.Model
+{
+    public class Car
+    {
+        public int Id { get; set; }
+        public string Brand { get; set; }
+        public string Type { get; set; }
+        public string License { get; set; }
+        public int Date { get; set; }
+    }
+}
